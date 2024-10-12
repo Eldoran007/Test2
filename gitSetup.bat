e9e7b4ae74b720133b414bf387fc5af8314540fb
Eldoran007 <dev.felixbecker@gmail.com>
2024-10-12 14:54:35 +0200

    Test

---
+++ gitSetup.bat
@@ -0,0 +1,8 @@
+git config --global user.email "dev.felixbecker@gmail.com"
+git config --global user.name "Eldoran007"
+git init
+git remote add origin https://github.com/Eldoran007/Test2.git
+git branch -M master
+git pull origin/master
+
+pause
